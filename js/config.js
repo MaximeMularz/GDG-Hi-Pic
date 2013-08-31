8b6df6178506810e127067ae92b14f00527bb3de
maxime <maxime@maxime.(none)>
2013-08-31 12:54:55 +0200

    first commit

---
+++ js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config',function(){
+    return {
+        //modify these
+        'name'          : 'GDG Hi Pic',
+        'id'            : '113891110741520781629',
+        'google_api'    : 'AIzaSyDzxj7Y8GJSLTtlHvQXDG_d9D9f1oINjwM',
+        'pwa_id'        : '5751686495430491313', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'DevFest Hi Pic',
+            subtitle : 'Google Developer Group de Haute Picardie',
+            button : {
+                text : 'Inscription',
+                url : 'https://plus.google.com/b/113891110741520781629/events/c2uomgabvv0r059n6svr189hvkk'
+            }
+        }
+    }
+});
